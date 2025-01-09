85eb807bd93fd2d4d4347e34f1d70ac0f61d404b
Lakshya Tyagi <51742210+eltyagi@users.noreply.github.com>
2025-01-09 19:50:36 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-    extends: 'next/core-web-vitals',
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2020,
+    "sourceType": "module"
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "rules": {
+    "no-unused-vars": "warn",
+    "no-console": "off"
+  },
+  "linterOptions": {
+    "reportUnusedDisableDirectives": true
+  }
 };
